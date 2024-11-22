0d26385c3f12d1b3db56750efdb90263eed5d51e
Blitzmachine <Blitzm4ch1n3@gmail.com>
2024-11-22 13:57:17 +0100

    DSC configuration

--- Plugins/DisplaySlashCommands.cs
+++ Plugins/DisplaySlashCommands.cs
@@ -1,10 +1,7 @@
-using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Text;
 using System;
-using Oxide.Core;
-using UnityEngine;
-using System.Text.Json.Serialization;
-using Epic.OnlineServices.TitleStorage;
+using Newtonsoft.Json;
 
 namespace Oxide.Plugins;
 
@@ -14,6 +11,8 @@ namespace Oxide.Plugins;
 public class DisplaySlashCommands : CovalencePlugin
 {
     #region Plugin Setup
+
+    private PluginConfiguration PluginConfig;
     private const string PERMISSION_NAME = "displayslash.use";
     #endregion
 
@@ -21,26 +20,48 @@ public class DisplaySlashCommands : CovalencePlugin
     private class PluginConfiguration
     {
 
-        [JsonProperty(propertyName: "List of external Plugins")]
-        public IEnumerable<ExternalPlugin> ListOfExternalPlugins { get; set; }
+        [JsonProperty(propertyName: "Sequence of External Plugins")]
+        public IEnumerable<ExternalPlugin> SeqOfExternalPlugins { get; set; }
         public static PluginConfiguration GetDefaultConfig() => new()
         {
-            ListOfExternalPlugins = new List<ExternalPlugin>
+            SeqOfExternalPlugins = new List<ExternalPlugin>
             {
                 {
                     new ExternalPlugin {
                         Title = "My Plugin XY",
-                        ListOfExternalCommands = new List<ExternalCommand>
+                        SeqOfExternalCommands = new List<ExternalCommand>
                         {
                             new ExternalCommand
                             {
                                 Name = "dostuff",
-                                Description = "Will do stuff!"
+                                Description = "Will do stuff!",
+                                Args = new List<ExternalCommandArgument>
+                                {
+                                    {
+                                        new ExternalCommandArgument
+                                        {
+                                            Position = 0,
+                                            Optional = false,
+                                            Argument = "player",
+                                            Description = "The player you want to target!"
+                                        }
+                                    },
+                                    {
+                                        new ExternalCommandArgument
+                                        {
+                                            Position = 1,
+                                            Optional = true,
+                                            Argument = "color",
+                                            Description = "Will send the message with color to the target!"
+                                        }
+                                    }
+                                }
                             },
                             new ExternalCommand
                             {
                                 Name = "dostuffhelp",
-                                Description = "Will display how to do stuff!"
+                                Description = "Will display how to do stuff!",
+                                Args = new List<ExternalCommandArgument>()
                             }
                         }
                     }
@@ -48,12 +69,13 @@ public class DisplaySlashCommands : CovalencePlugin
                 {
                     new ExternalPlugin {
                         Title = "EpicPlugin",
-                        ListOfExternalCommands = new List<ExternalCommand>
+                        SeqOfExternalCommands = new List<ExternalCommand>
                         {
                             new ExternalCommand
                             {
                                 Name = "epicstuff",
-                                Description = "Executing this command will do some epic stuff!"
+                                Description = "Executing this command will do some epic stuff!",
+                                Args = new List<ExternalCommandArgument>()
                             }
                         }
                     }
@@ -67,16 +89,55 @@ public class DisplaySlashCommands : CovalencePlugin
         [JsonProperty(propertyName: "Plugin Name")]
         public string Title { get; set; }
 
-        [JsonProperty(propertyName: "Included commands")]
-        public IEnumerable<ExternalCommand> ListOfExternalCommands { get; set; }
+        [JsonProperty(propertyName: "Commands")]
+        public IEnumerable<ExternalCommand> SeqOfExternalCommands { get; set; }
     }
 
     private class ExternalCommand
     {
         [JsonProperty(propertyName: "Command Name (without slash)")]
         public string Name { get; set; }
+
         [JsonProperty(propertyName: "Description")]
         public string Description { get; set; }
+
+        [JsonProperty(propertyName: "Command Arguments")]
+        public IEnumerable<ExternalCommandArgument> Args { get; set; }
+    }
+
+    private class ExternalCommandArgument
+    {
+        [JsonProperty(propertyName: "Position")]
+        public int Position { get; set; }
+
+        [JsonProperty(propertyName: "Argument is optional")]
+        public bool Optional { get; set; }
+
+        [JsonProperty(propertyName: "Argument Name")]
+        public string Argument { get; set; }
+
+        [JsonProperty(propertyName: "Argument Description")]
+        public string Description { get; set; }
+    }
+
+    protected override void SaveConfig() => Config.WriteObject(PluginConfig, true);
+    protected override void LoadDefaultConfig() => PluginConfig = PluginConfiguration.GetDefaultConfig();
+    protected override void LoadConfig()
+    {
+        base.LoadConfig();
+        try
+        {
+            PluginConfig = Config.ReadObject<PluginConfiguration>();
+        }
+        catch (Exception e)
+        {
+            var builder = new StringBuilder("Configuration file is corrupt - (Invalid JSON Format?)");
+            builder.AppendLine(e.Message);
+
+            PrintError(builder.ToString());
+            PrintWarning("Configuration Object is using default values.");
+            LoadDefaultConfig();
+        }
     }
     #endregion
 }
